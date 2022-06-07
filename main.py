bc06eef5e2ec522b674696d9eff58f4a6bd87d63
OranBourak <93316517+OranBourak@users.noreply.github.com>
2022-06-07 18:20:49 +0300

    Done trapezoidal method Add find required partition method Add find error precision method

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import math
+import sympy as sp
+from trapezoidal_integration import *
+from sympy.utilities.lambdify import lambdify
 
+# x = sp.symbols('x')
+# # f = math.sin
+# f = 2*x
+# f = lambdify(x, f)
+# f_2 =x**3
+# f_2_tag = sp.diff(sp.diff(f_2,x),x)
+# f_2_tag = lambdify(x,f_2_tag)
+
+if __name__ == "__main__":
+    print(trapezoidal_integration(f, 4, 0, 2))
+    print(find_required_partitions(f_2_tag,0.000002,0,math.pi))
+    print(find_error_presicion(f_2_tag,1137,0,math.pi))
--- trapezoidal_integration.py
+++ trapezoidal_integration.py
@@ -1,19 +1,24 @@
 import math
-from functools import reduce
+
 import sympy as sp
 
-x = sp.symbols('x')
-my_f = math.sin
-f = sp.lambdify(x, my_f)
 
-
-
-
-
-def trapezoidal_integration(function,n, start_point, end_point):
-    h = abs(end_point-start_point)/n
-    I = h *(0.5*(function(start_point)+function(end_point)) +reduce(lambda x, y:function(x)+function(y), [start_point+h*i for i in range(1, n)]))
+def trapezoidal_integration(function, start_point, end_point, n=0):
+    if (n <= 0):
+        n = 100
+    h = abs(end_point - start_point) / n
+    I = h * (0.5 * (function(start_point) + function(end_point)) + sum(
+        map(lambda x: function(x), [start_point + h * i for i in range(1, n)])))
     return I
 
 
-print(trapezoidal_integration(f,4,0,math.pi))
+def find_required_partitions(second_derivative,required_epsilon,start_point,end_point) :
+    maximum_derivative = max(map(lambda x: second_derivative(x), [start_point + 0.1 * i for i in range(0, int((end_point-start_point)/0.1))]))
+    n = math.sqrt(abs((end_point-start_point)**3 * maximum_derivative / (12*required_epsilon)))
+    return math.ceil(n)
+
+
+def find_error_precision(second_derivative,n,start_point,end_point):
+    maximum_derivative = max(map(lambda x: second_derivative(x), [start_point + 0.1 * i for i in range(0, int((end_point - start_point) / 0.1))]))
+    return abs((end_point-start_point)**3*maximum_derivative/(12*(n**2)))
+
